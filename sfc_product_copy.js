be3b2e06af4c1b86d269fa8b01035ab05057b4dc
Mikelarg <murtazkorpaz@gmail.com>
2015-07-04 12:30:38 +0800

    Added UserScript

---
+++ sfc_product_copy.js
@@ -0,0 +1,29 @@
+function generate_product_string() {
+	var products = [];
+	jQuery('textarea, input[type=text]').each(function(index) {
+		var product = new Object();
+		product.name = jQuery(this).attr('name');
+		product.value = jQuery(this).val();
+		products[index] = product;
+	});
+	return JSON.stringify(products);
+}
+
+function set_product_from_string(jsonString) {
+	var products = JSON.parse(jsonString);
+    for (var i = 0; i < products.length; i++) {
+    	var product = products[i];
+    	jQuery('textarea[name='+product.name+'], input[name='+product.name+']').val(product.value);
+    }
+}
+jQuery(document).ready(function() {
+	jQuery('body').prepend('<div id="copy_button" style="cursor: pointer; position: fixed;background-color: black;color: white;padding: 5px 20px; border-radius: 2px;  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.39); z-index: 1000; right: 0.5%; bottom: 5px;">Copy</div>');
+	jQuery('body').prepend('<div id="set_button" style="cursor: pointer; position: fixed;background-color: black;color: white;padding: 5px 20px; border-radius: 2px;  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.39); right: 0.5%; z-index: 1000; bottom: 40px;">Set</div>');
+	jQuery('#copy_button').click(function () {
+		window.prompt("Copy to clipboard: Ctrl+C, Enter", generate_product_string());
+	});
+	jQuery('#set_button').click(function () {
+		var productString = window.prompt("Set product string", "");
+		set_product_from_string(productString);
+	});
+});
