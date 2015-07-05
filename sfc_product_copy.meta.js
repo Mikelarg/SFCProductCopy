522bc408e663d956af2cff1b02c3b9b7a9fefa4e
Mikelarg <murtazkorpaz@gmail.com>
2015-07-05 10:09:30 +0800

    Added new domain off send from china

--- sfc_product_copy.meta.js
+++ sfc_product_copy.meta.js
@@ -4,6 +4,7 @@
 // @description  Product Copy SFC
 // @author       Mikelarg
 // @match        https://fulfill.sfcservice.com/*
+// @match        https://fulfill.sendfromchina.com/*
 // @require http://code.jquery.com/jquery-latest.js
 // @updateURL		https://raw.githubusercontent.com/Mikelarg/SFCProductCopy/master/sfc_product_copy.meta.js
 // @downloadURL		https://raw.githubusercontent.com/Mikelarg/SFCProductCopy/master/sfc_product_copy.user.js
--- sfc_product_copy.user.js
+++ sfc_product_copy.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Mikelarg copy product SFC
-// @version      1.0.1
+// @version      1.0.2
 // @description  Product Copy SFC
 // @author       Mikelarg
 // @match        https://fulfill.sfcservice.com/*
+// @match        https://fulfill.sendfromchina.com/*
 // @require http://code.jquery.com/jquery-latest.js
 // @updateURL		https://raw.githubusercontent.com/Mikelarg/SFCProductCopy/master/sfc_product_copy.meta.js
 // @downloadURL		https://raw.githubusercontent.com/Mikelarg/SFCProductCopy/master/sfc_product_copy.user.js
